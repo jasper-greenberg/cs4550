00650a4864e3c11038ad3beed879900adddc7d35
Jasper Greenberg <jaspergreenberg@gmail.com>
2024-03-20 18:19:41 -0400

    commit for a4

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ClickEvent from "./ClickEvent";
+
+const Assignment4 = () => {
+    return (
+        <>
+            <h1>Assignment 4</h1>
+
+            <ClickEvent />
+
+        </>
+    );
+};
+
+export default Assignment4;
