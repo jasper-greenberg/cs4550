d5bd1322fb8be870ff7930af6d2fcd0babb9402d
Jasper Greenberg <jaspergreenberg@gmail.com>
2024-04-12 19:50:28 -0400

    fixed infinite rendering on edit page

--- src/Kanbas/Courses/Quizzes/edit.tsx
+++ src/Kanbas/Courses/Quizzes/edit.tsx
@@ -11,32 +11,32 @@ export default function QuizEdit() {
     const { quizId } = useParams();
     const [quiz, setQuiz] = useState<Quiz>(state?.quiz);
 
-    // if quizId is not provided, create a new quiz
-    if (!quizId) {
-        setQuiz({
-            _id: "",
-            title: "",
-            description: "",
-            course_id: "",
-            published: false,
-            type: "Graded Quiz",
-            group: "Quizzes",
-            shuffle_answers: true,
-            time_limit: 20,
-            multiple_attempts: false,
-            show_correct_answers: "After last attempt",
-            access_code: "",
-            one_question_at_a_time: true,
-            webcam_required: false,
-            lock_questions_after_answering: false,
-            due_date: new Date(),
-            available_date: new Date(),
-            available_until_date: new Date(),
-            questions: [],
-        });
-    }
-
     useEffect(() => {
+        // if quizId is not provided, create a new quiz
+        if (!quizId) {
+            setQuiz({
+                _id: "",
+                title: "",
+                description: "",
+                course_id: "",
+                published: false,
+                type: "Graded Quiz",
+                group: "Quizzes",
+                shuffle_answers: true,
+                time_limit: 20,
+                multiple_attempts: false,
+                show_correct_answers: "After last attempt",
+                access_code: "",
+                one_question_at_a_time: true,
+                webcam_required: false,
+                lock_questions_after_answering: false,
+                due_date: new Date(),
+                available_date: new Date(),
+                available_until_date: new Date(),
+                questions: [],
+            });
+        }
+
         const fetchQuiz = async () => {
             if (quizId) {
                 const quiz = await client.findQuizById(quizId);
